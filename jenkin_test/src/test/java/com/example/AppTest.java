f337f0319353e019ef62cd01e7385e5c1b44b07c
neojarvis <techadmin@iamneo.ai>
2025-07-11 06:05:22 +0000

    jenkins commit

--- jenkin_test/src/test/java/com/example/AppTest.java
+++ jenkin_test/src/test/java/com/example/AppTest.java
@@ -15,7 +15,6 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        System.out.println(assertTrue( true ));
         System.out.println("Sample jenkin mari");
     }
 }
